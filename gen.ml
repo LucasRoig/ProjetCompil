cc0117a0b603c9f620af6acf6a871b2811778ffb
Lucas Roig <lucas1.roig@gmail.com>
2017-02-13 18:26:23 +0100

    La fonction gen_expr gere maintenant les branchements et les appels de fonction. Il faut encore se pencher sur le cas des variables globales. De plus rien n'a ete teste.

--- gen.ml
+++ gen.ml
@@ -4,6 +4,10 @@ open Lang
 open Analyses
 open Instrs
 
+let compte_labels = ref 0;;
+let incr_cpt_label () =
+  incr compte_labels;[!compte_labels];; (*On a apparement besoin d'une liste d'int*)
+
 (* ************************************************************ *)
 (* **** Compilation of expressions / statements            **** *)
 (* ************************************************************ *)
@@ -13,6 +17,7 @@ let position e list =
     | [] -> failwith "position : l'element n'est pas dans la liste, le typage ne fonctionne pas" in
   pos 0 e list;;
 
+(*Le cas des variables globales est a gerer*)
 let rec gen_exp varList = function
     Const(IntT,v) -> [Loadc(IntT,v)]
   | Const(BoolT,BoolV t) ->
@@ -23,14 +28,23 @@ let rec gen_exp varList = function
      let l1 = gen_exp varList exp1 and l2 = gen_exp varList exp2 in
      begin match op with
        BArith _ | BLogic _ -> l1@l2@[Bininst(IntT,op)]
-     | BCompar _ ->  failwith "gen_exp: une comparaison necessite un branchement et n'est pas encore implemente"
+     | BCompar opComp ->
+        let lbl_true = incr_cpt_label () and lbl_fin = incr_cpt_label () in
+        l1@l2@If(opComp,lbl_true)::Loadc(IntT,IntV 0)::Goto lbl_fin::Label lbl_true::Loadc(IntT,IntV 1)::[Label lbl_fin]
      end
   | IfThenElse(_,exp1,exp2,exp3) ->
-     failwith "gen_exp: IfThenElse necessite un branchement et n'est pas encore implemente"
+     let lbl_false = incr_cpt_label () and lbl_fin = incr_cpt_label () in
+     (gen_exp varList exp1)@
+     [Loadc(IntT,IntV 0);If(BCeq,lbl_false)]@
+     (gen_exp varList exp2)@[Goto lbl_fin]@
+     (Label lbl_false::(gen_exp varList exp3)@[Label lbl_fin])
   | CallE(t,name,expList) ->
-     failwith "l'appel de fonction n'est pas encore implemente"
-  | Const (VoidT,_) ->
-     failwith "Une constante ne devrait pas avoir pour type void, le typage ne marche pas"
+     let push_args = List.concat (List.map (gen_exp varList) expList) in
+     let ret_type = if t = BoolT then IntT else t in
+     let args_tps = List.map (function BoolT -> IntT | t -> t) (List.map tp_of_expr expList) in
+     push_args@[Invoke(ret_type,name,args_tps)]
+  | _ ->
+     failwith "Le filtrage de gen_exp n'as pas fonctionne, il y a certainement une erreur de typage"
 ;;
 
 
